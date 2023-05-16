56990e76851fe3608d7ff176b2691024d50fa485
Igor Burenkov <49118930+BrNkV@users.noreply.github.com>
2023-05-17 02:56:22 +0300

    feat: add text message + open.ai

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
-import { Telegraf } from 'telegraf';
+import { Telegraf, session } from 'telegraf';
 
 // фильтры телеграфа
 import { message } from 'telegraf/filters';
+
+// форматтер стилистики текста
+import { code } from 'telegraf/format';
 
 // подключение токенов и констант (файл default.json)
 import config from 'config';
@@ -10,18 +13,43 @@ import { ogg } from './ogg.js';
 
 import { openai } from './openai.js';
 
+// для каждого пользователя своя сессия
+const INITIAL_SESSION = {
+  message: [],
+};
+
 //создаем бота (принимает токен телеги)
 const bot = new Telegraf(config.get('TELEGRAM_TOKEN'));
+
+// бот будет использовать сессии
+bot.use(session());
+
+// при вводе /new будет создаваться новый контекст(новая беседа) (если нет то диалог продолжается)
+bot.command('new', async (ctx) => {
+  ctx.session = INITIAL_SESSION;
+  await ctx.reply(code('Жду Ваше сообщение голосом или текстом...'));
+});
+
+// обработка команды start
+bot.command('start', async (ctx) => {
+  ctx.session = INITIAL_SESSION;
+  await ctx.reply(code('Жду Ваше сообщение голосом или текстом...'));
+});
 
 // // обработка текстовой команды
 // bot.on(message('text'), async (ctx) => {
 //   await ctx.reply(JSON.stringify(ctx.message, null, 2));
 // });
 
-// обработка голосовой команды
+// обработка голосовой команды VOICE
 bot.on(message('voice'), async (ctx) => {
+  // если сессия не определена (если сессия = null или undef тогда INITIAL_SESSION)
+  ctx.session ??= INITIAL_SESSION;
   try {
     // await ctx.reply(JSON.stringify(ctx.message.voice, null, 2));
+    await ctx.reply(
+      code('Сообщение принято к обработке. Ожидается ответ от сервера...'),
+    );
 
     // 1 - получим ссылку на голосовой файл
     const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id);
@@ -36,19 +64,61 @@ bot.on(message('voice'), async (ctx) => {
 
     // получаем текст (обработка openAi - принимает к обработке наш мп3)
     const text = await openai.transcription(mp3Path);
-    // затем openai определенный текст передаст в чат
-    const response = await openai.chat(text);
+    await ctx.reply(code(`Ваш запрос: ${text}`));
 
-    // ответ бота
-    await ctx.reply(mp3Path);
+    // затем openai определенный текст передаст в чат
+    // переделаем на messages
+    ctx.session.messages.push({ role: openai.roles.USER, content: 'text' });
+    // получаем ответ
+    const response = await openai.chat(ctx.session.messages);
+
+    ctx.session.messages.push({
+      role: openai.roles.ASSISTANT,
+      content: response.content,
+    });
+
+    await ctx.reply(response.content);
+
+    // ответ бота (передаем "text" - когда на Api есть деньги)
+    //FIXME оплатить OpenAi API передать "text"
+    await ctx.reply(mp3Path); //можно убрать
   } catch (e) {
     console.log('Error while voice message', e.message);
   }
 });
 
-// обработка команды start
-bot.command('start', async (ctx) => {
-  await ctx.reply(JSON.stringify(ctx.message, null, 2));
+// обработка текстовой команды TEXT
+bot.on(message('text'), async (ctx) => {
+  // если сессия не определена (если сессия = null или undef тогда INITIAL_SESSION)
+  ctx.session ??= INITIAL_SESSION;
+  try {
+    // await ctx.reply(JSON.stringify(ctx.message.voice, null, 2));
+    await ctx.reply(
+      code('Сообщение принято к обработке. Ожидается ответ от сервера...'),
+    );
+
+    // затем openai определенный текст передаст в чат
+    // переделаем на messages
+    ctx.session.messages.push({
+      role: openai.roles.USER,
+      content: ctx.message.text,
+    });
+    // получаем ответ
+    const response = await openai.chat(ctx.session.messages);
+
+    ctx.session.messages.push({
+      role: openai.roles.ASSISTANT,
+      content: response.content,
+    });
+
+    await ctx.reply(response.content);
+
+    // ответ бота (передаем "text" - когда на Api есть деньги)
+    //FIXME оплатить OpenAi API передать "text"
+    await ctx.reply(mp3Path); //можно убрать
+  } catch (e) {
+    console.log('Error while text message', e.message);
+  }
 });
 
 // запуск бота
--- src/openai.js
+++ src/openai.js
@@ -1,8 +1,15 @@
 // подключаем OpenAi API
 import { Configuration, OpenAIApi } from 'openai';
 import config from 'config';
+import { createReadStream } from 'fs';
 
 class OpenAi {
+  roles = {
+    ASSISTANT: 'assistant',
+    USER: 'user',
+    SYSTEM: 'system: ',
+  };
+
   constructor(apiKey) {
     const configuration = new Configuration({
       apiKey,
@@ -10,9 +17,35 @@ class OpenAi {
     this.openai = new OpenAIApi(configuration);
   }
 
-  chat() {}
+  async chat(messages) {
+    try {
+      // передаем модель
+      // передаем объект messages(массив сообщения (роль{system, user, assistant(ответ GPT чата или контекст диалога)}, контент, имя?))
+      const response = await this.openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages,
+      });
+      // распарсиваем поступающий JSON
+      return response.data.choices[0].message
+    } catch (e) {
+      console.log('Error while GPTchat' + e.message);
+    }
+  }
 
-  transcription() {}
+  async transcription() {
+    try {
+      // (1)передаем файл(не путь) / один из вариантов создать стрим
+      //(2) передаем модель
+      const response = await this.openai.createTranscription(
+        createReadStream(filepath),
+        'whisper-1',
+      );
+      // возврат ответа с текстом
+      return response.data.text;
+    } catch (e) {
+      console.log('Error while transcription' + e.message);
+    }
+  }
 }
 
 export const openai = new OpenAi(config.get('OPENAI_KEY'));
